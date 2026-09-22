f6c2f7fb1663d7e975ae1f702e47897399af2cb2
unknown <eriklimagao1@gmail.com>
2026-09-22 09:40:06 -0300

    feat: adiciona formulario de pendencias

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,24 @@
 import { useState } from "react"
 
 function App() {
-  const[cliente, steCliente] = useState("");
+  const[cliente, setCliente] = useState("");
   const[servicos, setServicos] = useState([]);
   const[novoServico, setNovoServico] = useState("");
+  const[pendencias, setPendencias] = useState("");
+  const[novaPendencia, setNovaPendencia] = useState("");
+  const[precisaPeca, setPrecisaPeca] = useState(null);
+  const[peca, setPeca] = useState("");
+  const[pecaDisponivel, setPecaDisponivel] = useState(null);
+  const adicionarServico = () => {
+    if (novoServico.trim() === "") {
+      return;
+    }
+    setServicos([...servicos, novoServico]);
+    setNovoServico("");
+  }; 
+  const removerServico = (index) => {
+    setServicos(servicos.filter((_, i) => i !== index));
+  };
   return(
     <div className="min-h-screen bg-gray-300 p-6">
       <div className="max-auto max-w-5x1">
@@ -45,9 +60,25 @@ function App() {
                 placeholder="Descreva o serviço realizado"
                 className="mb-3 w-full-md rounded-lg border border-gray-300 bg-white px-4 py-2 shadow-md outline-none focus:border-blue-500"
               />
-              <button className="rounded bg-blue-600 px-4 py-2 text-white hover:bg-green-700">
+              <button 
+                onClick={adicionarServico}
+                className="rounded bg-blue-600 px-4 py-2 text-white hover:bg-green-700">
                 + Adicionar Serviços
               </button>
+              {servicos.map((servico, index) => (
+                <div 
+                key={index}
+                className="rounded-lg bg-gray-100 px-4 py-3 text-sm text-gray-700"
+                >
+                  <span>{servico}</span>
+
+                  <button
+                    onClick={() => removerServico(index)}
+                    className="text-red-500 hover:text-red-700">
+                    Remover
+                  </button>
+                </div>
+              ))}
             </div>
           </section>
 
@@ -59,9 +90,85 @@ function App() {
               Registre os serviços que ficaram pendentes.
             </p>
 
-            <button className="rounded bg-blue-600 px-4 py-2 text-white hover:bg-orange-700">
-              + Adicionar Pendências
-            </button>
+            <div className="flex flex-col gap-3">
+
+              <input 
+                type="text" 
+                value={novaPendencia}
+                onChange={(e) => setNovaPendencia(e.target.value)}
+                placeholder="Descreva a pendência"
+                className="w-full rounded-lg border border-gray-300 bg-white px-4 py-2 
+                shadow-md outline-none focus:border-blue-500"
+              />
+
+              <label className="text-sm font-medium text-gray-700">
+                Precisa Peça?
+              </label>
+
+              <div className="flex gap-4">
+                <label className="flex items-center gap-2">
+                  <input 
+                    type="radio"
+                    name="precisaPeca" 
+                    checked={precisaPeca === true}
+                    onChange={() => setPrecisaPeca(true)}
+                  />
+                  Sim
+                </label>
+
+                <label className="flex items-center gap-2">
+                  <input 
+                    type="radio"
+                    name="precisaPeca" 
+                    checked={precisaPeca === false}
+                    onChange={() => setPrecisaPeca(false)}
+                  />
+                  Não
+                </label>
+              </div>
+              
+              {precisaPeca === true && (
+                <>
+                  <input
+                    type="text"
+                    value={peca}
+                    onChange={(e) => setPeca(e.target.value)}
+                    placeholder="Digite o nome da peça"
+                    className="w-full rounded-lg border border-gray-300 bg-white px-4 py-2 shadow-md outline-none focus:border-blue-500"
+                  />
+
+                  <label className="text-sm font-medium text-gray-700">
+                    Peça disponível?
+                  </label>
+
+                  <div className="flex gap-4">
+                    <label className="flex items-center gap-2">
+                      <input
+                      type="radio"
+                        name="pecaDisponivel"
+                        checked={pecaDisponivel === true}
+                        onChange={() => setPecaDisponivel(true)}
+                      />
+                      Sim
+                    </label>
+
+                    <label className="flex items-center gap-2">
+                      <input
+                        type="radio"
+                        name="pecaDisponivel"
+                        checked={pecaDisponivel === false}
+                        onChange={() => setPecaDisponivel(false)}
+                      />
+                        Não
+                    </label>
+                  </div>
+                </>
+              )}
+
+              <button className="rounded bg-blue-600 px-4 py-2 text-white hover:bg-orange-700">
+                + Adicionar Pendências
+              </button>
+            </div>
           </section>
         </div>
 
